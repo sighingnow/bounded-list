d5b9de6d5514fbbd4748a261b20324f9ce437b58
sighingnow <sighingnow@gmail.com>
2016-10-03 11:01:16 +0800

    Instance (BList n) as Foldable, Show and Eq.

--- src/Data/BList.hs
+++ src/Data/BList.hs
@@ -16,32 +16,44 @@ import Prelude hiding ((++))
 import Data.BList.Nat
 
 -- | Type-safe list.
-data BList a n =
+data BList n a =
     (n ~ O) => Nil
-    | forall m. n ~ S m => (:+) a (BList a m)
+    | forall m. n ~ S m => (:+) a (BList m a)
 
 infixr 5 :+
 
+instance (Show a) => Show (BList n a) where
+    show = show . toList
+
+instance (Eq a) => Eq (BList n a) where
+    (==) Nil Nil        = True
+    (==) (x:+xs) (y:+ys) = x == y && xs == ys
+    (==) _ _            = False
+
+instance Foldable (BList n) where
+    foldMap f Nil     = mempty
+    foldMap f (x:+xs) = mappend (f x) (foldMap f xs)
+
 -- | Construct an empty BList.
-empty :: BList a O
+empty :: BList O a
 empty = Nil
 
 -- | Construct a BList from only one element.
-singleton :: a -> BList a (S O)
+singleton :: a -> BList (S O) a
 singleton x = x :+ Nil
 
 -- | Append operation.
-(++) :: BList a n -> BList a m -> BList a (n .+. m)
+(++) :: BList n a -> BList m a -> BList (n .+. m) a
 (++) Nil ts     = ts
 (++) (x:+xs) ts = x :+ (++) xs ts
 
-infixr 5 ++
-
 -- | Transform BList to ordinary list.
-toList :: BList a n -> [a]
+toList :: BList n a -> [a]
 toList Nil     = []
 toList (x:+xs) = x : toList xs
 
 -- | Transform ordinary list to BList.
-fromList :: [a] -> BList a n
+fromList :: [a] -> BList n a
 fromList = undefined -- foldr (\x l -> x :+ l) Nil
+
+
