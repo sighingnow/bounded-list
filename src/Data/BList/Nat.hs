d052195608089390fe4f7dfc0312e0b519a1e997
sighingnow <sighingnow@gmail.com>
2016-10-03 11:02:29 +0800

    Add singleton promoted type for Nat.

--- src/Data/BList/Nat.hs
+++ src/Data/BList/Nat.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -7,20 +8,43 @@ module Data.BList.Nat (
       Nat (..)
     , type (.+.)
     , type (.*.)
+    , SNat (..)
+    , (.:+)
+    , (.:*)
 ) where
 
 -- | Inductive natural number.
 data Nat = O | S Nat
 
-infixl 6 .+.
-infixl 7 .*.
-
--- | addition operation for natural number.
+-- | Addition operation for natural number.
 type family (n :: Nat) .+. (m :: Nat) :: Nat
 type instance O .+. m     = m
 type instance (S n) .+. m = S (n .+. m)
 
--- | multiplication operation for natural number.
+infixl 6 .+.
+
+-- | Multiplication operation for natural number.
 type family (n :: Nat) .*. (m :: Nat) :: Nat
 type instance O .*. m     = O
 type instance (S n) .*. m = (n .*. m) .+. m
+
+infixl 7 .*.
+
+-- | Using singleton to promoting the origin Nat type.
+data SNat n where
+    SO :: SNat O
+    SS :: SNat n -> SNat (S n)
+
+-- | Addition operation for singleton Nat type.
+(.:+) :: SNat n -> SNat m -> SNat (n .+. m)
+SO   .:+ m = m
+SS n .:+ m = SS (n .:+ m)
+
+infixl 6 .:+
+
+-- | Multiplication operation for singleton Nat type.
+(.:*) :: SNat n -> SNat m -> SNat (n .*. m)
+SO   .:* m = SO
+SS n .:* m = (n .:* m) .:+ m
+
+infixl 7 .:*
